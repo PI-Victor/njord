f4771307838ddefa6f5606ea290d53fe3c2e0391
Victor Palade <victor@cloudflavor.io>
2019-10-08 11:12:09 +0200

    cargo: update build script

--- build.rs
+++ build.rs
@@ -3,12 +3,26 @@ extern crate protoc_rust;
 use protoc_rust::Customize;
 
 fn main() {
-	protoc_rust::run(protoc_rust::Args {
-	    out_dir: "src/protos",
-	    input: &["protobuf/node.proto"],
-	    includes: &["protobuf"],
-	    customize: Customize {
-	      ..Default::default()
-	    },
-	}).unwrap();
+    protoc_rust::run(protoc_rust::Args {
+        out_dir: "src/protos/internal",
+        input: &[
+            "protobuf/internal/node.proto",
+            "protobuf/internal/registry.proto",
+        ],
+        includes: &["protobuf/"],
+        customize: Customize {
+            ..Default::default()
+        },
+    })
+    .unwrap();
+
+    // protoc_rust::run(protoc_rust::Args {
+    //     out_dir: "assets/clients",
+    //     input: &[],
+    //     includes: &["protobuf"],
+    //     customize: Customize {
+    //         ..Default::default()
+    //     },
+    // })
+    // .unwrap();
 }
