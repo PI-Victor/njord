6154912ebca2d57bc71d6de23cb5376d655d86cc
Victor Palade <victor@cloudflavor.io>
2019-10-20 21:32:45 +0200

    deps: update
    * clean up protobuf generation

--- build.rs
+++ build.rs
@@ -5,24 +5,11 @@ use protoc_rust::Customize;
 fn main() {
     protoc_rust::run(protoc_rust::Args {
         out_dir: "src/protos/internal",
-        input: &[
-            "protobuf/internal/node.proto",
-            "protobuf/internal/registry.proto",
-        ],
+        input: &["protobuf/internal/registry.proto"],
         includes: &["protobuf/"],
         customize: Customize {
             ..Default::default()
         },
     })
     .unwrap();
-
-    // protoc_rust::run(protoc_rust::Args {
-    //     out_dir: "assets/clients",
-    //     input: &[],
-    //     includes: &["protobuf"],
-    //     customize: Customize {
-    //         ..Default::default()
-    //     },
-    // })
-    // .unwrap();
 }
